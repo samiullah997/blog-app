0887d675acd2f826d3dd4db3f89a4c011e422f15
samiullah997 <samiullahk997@gmail.com>
2022-12-24 01:55:08 +0500

    Create Comments_controller and added their functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,35 +1,35 @@
 class CommentsController < ApplicationController
+
+  # get posts and comments for show action
   def new
-    @comment = Comment.new
     @post = Post.find(params[:post_id])
+    @comment = Comment.new
   end
 
+  # create a new comment
   def create
     post = Post.find(params[:id])
     comment = Comment.create(author: current_user, post:, **comment_params)
-
-    if comment.save
-      flash[:success] = 'Comment was successfully saved !'
-      redirect_to user_post_url(post.author, post)
-    else
+    unless comment.save
       redirect_to new_user_session_url
+    else
+      redirect_to user_post_url(post.author, post), notice: 'Comment was successfully created.'
     end
   end
 
+  # delete a comment
   def destroy
+    comment = Comment.find(params[:id])
     user = User.find(params[:user_id])
     post = user.posts.find(params[:post_id])
-    comment = Comment.find(params[:id])
-    if comment.destroy
-      flash[:success] = 'Comment was successfully deleted.'
-    else
-      flash[:error] = 'Error: Comment could not be deleted'
-    end
+    comment.destroy ? flash[notice:] = 'Comment was successfully deleted.' : flash[notice:] = 'Error: Comment could not be deleted'
+
     redirect_to user_post_path(user, post)
   end
 
   private
 
+  # only allow a list of trusted parameters through
   def comment_params
     params.require(:comment).permit(:text)
   end
