0721a46b142a8435220491fc34d2b79b2226a8d4
samiullah997 <samiullahk997@gmail.com>
2022-12-24 01:55:53 +0500

    Create like_controller and added their functionality

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,12 @@
 class LikesController < ApplicationController
+
+  # This method is used to create a new like.
   def create
     post = Post.find(params[:post_id])
     like = Like.create(author: current_user, post:)
 
-    if like.save
-      flash[:success] = 'Post was liked'
-    else
-      flash.now[:error] = 'Error: Like could not be saved'
-    end
+    like.save ? flash[:notice] = 'Post was liked' : flash.now[:notice] = 'Error: Like could not be saved'
+
     redirect_to user_post_url(current_user.id, post.id)
   end
 end
