3901fb81fc31978142bd026ceb3831f5fca712c1
samiullah997 <samiullahk997@gmail.com>
2022-12-24 02:00:20 +0500

    create comments_controller and added their functionality

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::CommentsController < Api::V1::ApplicationController
-  before_action :set_post, only: %i[index show create]
   before_action :set_author, only: %i[index show create]
+  before_action :set_post, only: %i[index show create]
   before_action :set_comment, only: [:show]
 
   def index
@@ -13,28 +13,26 @@ class Api::V1::CommentsController < Api::V1::ApplicationController
   end
 
   def create
-    # should use current_user
     @comment = @author.comments.new(post: @post, **comment_params)
-    if @comment.save
-      render json: @comment, status: :created
-    else
+    unless @comment.save  
       render json: @comment.errors, status: :unprocessable_entity
+    else
+      render json: @comment, status: :created
     end
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
+  def set_comment
+    @comment = set_post.comments.find(params[:id])
+  end
+
   def set_author
     @author = User.find(params[:user_id])
   end
 
   def set_post
     @post = set_author.posts.find(params[:post_id])
-  end
-
-  def set_comment
-    @comment = set_post.comments.find(params[:id])
   end
 
   def comment_params
